888ebcc10f45245559cb6ac1a4b7fe6867355431
OgataiKhan <badellino.kevin@gmail.com>
2023-12-12 17:00:04 +0100

    feat: selector

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,37 @@
 <script>
+import MainArchetype from './MainArchetype.vue';
+import MainCards from './MainCards.vue';
 
 export default {
   name: 'Main',
-  
+  components: {
+    MainArchetype,
+    MainCards,
+  },
 };
 
 </script>
 
 <template>
   <main>
-    main
+    <div class="container">
+      <MainArchetype />
+      <MainCards />
+    </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
+@use '../assets/scss/partial/variables' as *;
 
+
+main {
+  background-color: $primary-color;
+
+  .container {
+    max-width: 1250px;
+    margin: 0 auto;
+    padding: 15px;
+  }
+}
 </style>
---
+++ src/components/MainArchetype.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default {
+  name: 'ArchetypeSelector',
+
+};
+
+</script>
+
+<template>
+  <label for="archetype-selector">Choose an archetype:</label>
+  <select name="archetype" id="archetype-selector">
+    <option value="alien">Alien</option>
+  </select>
+</template>
+
+<style lang="scss" scoped>
+label {
+  display: none;
+}
+
+#archetype-selector {
+  width: 163px;
+  border: 1px solid #CED4DA;
+  border-radius: 5px;
+  padding: 8px 15px 8px 10px;
+  margin: 0 10px;
+}
+</style>
