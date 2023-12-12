2f261a8afc875dae83a554ea1da63511a6efecd4
OgataiKhan <badellino.kevin@gmail.com>
2023-12-13 00:16:55 +0100

    feat: footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,17 +2,26 @@
 
 export default {
   name: 'Footer',
-  
+
 };
 
 </script>
 
 <template>
   <footer>
-    footer
+    <p>Made with &hearts; by Kevin</p>
   </footer>
 </template>
 
 <style lang="scss" scoped>
+@use '../assets/scss/partial/variables' as *;
+
+footer {
+  background-color: $secondary-color;
+  p {
+    text-align: center;
+    padding: 25px 0;
+  }
+}
 
 </style>
